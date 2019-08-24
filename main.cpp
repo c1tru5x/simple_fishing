8d450d4611cc47230375660fbe2594e9d014e0bf
Max Bauer <maxiangelo97@googlemail.com>
2019-08-25 01:47:21 +0200

    add basic ascii art thanks @chris

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <Windows.h>
 #include <ctime>
+#include "Screen.h"
 
 #include "fishing.h"
+#include "ResourceManager.h"
 
 using namespace std;
 
@@ -10,7 +12,24 @@ int main(void)
 {
     SetConsoleTitle("Simple_Fishing by c1tru5x");
 	//Init
-	PlayerObjects p;
-	p.gameStart();
+
+	ResourceManager resource_manager_;
+
+	Screen screen(resource_manager_);
+
+	screen.draw_fisher();
+
+	Sleep(1000);
+
+	screen.draw_fish();
+
+	Sleep(1000);
+
+	screen.draw_fisher_pull_out();
+
+	Sleep(1000);
+
+	//PlayerObjects p;
+	//p.gameStart();
 	return 0;
 }
